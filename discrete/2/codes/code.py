2a34b129a7cd51592a30ec8a0219820a6487c83f
KSBallvardhan <159809971+KSBallvardhan@users.noreply.github.com>
2024-02-29 20:05:02 +0530

    Update code.py

--- discrete/2/codes/code.py
+++ discrete/2/codes/code.py
@@ -8,13 +8,15 @@ data = np.loadtxt("values2.txt", skiprows=1)
 plt.close("all")
 
 # extract the 12 terms of the data
-x_n = data[:12]
+y_n = data[:12]
+highlighted_index = 11
 # plot the graph
-plt.stem(range(1, len(data) + 1), x_n, markerfmt='bo', linefmt='b-', basefmt='r-',label=r'Simulation') 
+plt.stem(range(1, len(data) + 1), y_n, markerfmt='bo', linefmt='b-', basefmt='r-',label=r'Simulation')
+plt.stem([highlighted_index], [data[highlighted_index - 1]], linefmt='r-', markerfmt='ro', basefmt=' ')
 plt.scatter(range(1, len(data) + 1), data, color='orange',marker='x',s=100,label=r'Analysis')
 # Set labels and title
 plt.xlabel('n')
-plt.ylabel('x(n)')
+plt.ylabel('y(n)')
 plt.xticks(range(1, len(data) + 1))
 # Add legend
 plt.legend()
