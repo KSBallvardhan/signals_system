975b993e1883a8b23fc58525f90ad02cf77736ea
KSBallvardhan <159809971+KSBallvardhan@users.noreply.github.com>
2024-02-29 14:48:01 +0530

    Update code.c

--- discrete/2/codes/code.c
+++ discrete/2/codes/code.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+// Function to generate multiples of 3 without indices
+void generateMultiplesOfThree(int n, FILE *file) {
+    int currentMultiple = 3;
+    fprintf(file, "Value\n");
+
+    while (n > 0) {
+        fprintf(file, "%d\n", currentMultiple);
+        currentMultiple += 3;
+        n--;
+    }
+}
+
+int main() {
+    // Open file for writing
+    FILE *file = fopen("values2.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file for writing.");
+        return 1; // Exit with an error code
+    }
+
+    // Generate and write the first 12 multiples of 3
+    generateMultiplesOfThree(12, file);
+
+    // Close the file
+    fclose(file);
+
+    printf("Multiples of 3 have been written to values2.txt.\n");
+
+    return 0;
+}
