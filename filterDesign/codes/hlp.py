c6af061130e4095fd7c5634f9b378f5eed2ed339
KSBallvardhan <159809971+KSBallvardhan@users.noreply.github.com>
2024-04-14 22:07:24 +0530

    Create hlp.py

---
+++ filterDesign/codes/hlp.py
@@ -0,0 +1,23 @@
+from sympy import symbols, simplify
+
+# Define symbolic variable s
+s = symbols('s')
+
+# Constants
+omega0_val = 0.3489
+B_val =0.0883
+
+# Given roots
+s1 = -0.1982 - 1.0404j
+s2 = -0.4785 - 0.4310j
+s3 = -0.4785 + 0.4310j
+s4 = -0.1982 + 1.0404j
+# Define the given polynomial expression
+polynomial_expr = 0.4480/ ((s - s1) * (s - s2) * (s - s3) * (s - s4))
+
+# Simplify the expression
+simplified_expr = simplify(polynomial_expr)
+
+# Print the simplified expression
+print("Simplified Polynomial in terms of s:")
+print(simplified_expr)
